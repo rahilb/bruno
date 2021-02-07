a63d7f00cfa6835b80a869ad6259b8e5173ee89f
Rahil Bohra <rahil@bohra.co.uk>
2021-02-07 17:56:19 +0000

    More coinbase operations

--- bruno.ml
+++ bruno.ml
@@ -1,8 +1,28 @@
-include Coinbase
+open Coinbase
 
 let () =
-  let accounts = Result.get_ok (Lwt_main.run Coinbase.get_accounts) in
-  print_endline (show_account_list accounts)
+  let accounts = match (Lwt_main.run get_accounts) with
+  | Result.Ok l -> l
+  | Result.Error e -> print_endline e; [] in
+  List.iter (fun (account: account) -> 
+    let () = print_endline (show_account account) in
+    let result = Lwt_main.run (get_account_history account.id) in 
+    match result with 
+    | Result.Ok l -> print_endline (show_account_history_list l)
+    | Result.Error e -> print_endline ("ERROR: " ^ e)
+  ) accounts
+
+let () =
+  let products = Lwt_main.run get_products in
+  match products with 
+  | Result.Ok p -> print_endline (show_product_list p)
+  | Result.Error e -> print_endline ("ERROR: " ^ e)
+
+let () =
+  let btc_gbp = Lwt_main.run (get_product "BTC-GBP") in
+  match btc_gbp with 
+  | Result.Ok p -> print_endline (show_product p)
+  | Result.Error e -> print_endline ("ERROR: " ^ e)
 
 let () = 
   print_endline "Goodbye, World!"
--- coinbase.ml
+++ coinbase.ml
@@ -24,7 +24,74 @@ type account = {
   trading_enabled : bool; 
 } [@@deriving show, yojson]
 
+type tx_type =
+| Transfer   [@name "transfer"]
+| Match [@name "match"]
+| Fee [@name "fee"]
+| Rebate [@name "rebate"]
+| Conversion [@name "conversion"]
+[@@deriving show, yojson]
+
+type account_history = {
+  id: string; 
+  created_at: string;  
+  amount: string; 
+  balance: string; 
+  typ: string [@key "type"]; 
+} [@@deriving show, yojson {strict = false}]
+
+type order_side =
+| Buy   [@name "buy"]
+| Sell [@name "sell"]
+[@@deriving show, yojson]
+
+type order = {
+  id: string;
+  price: string;
+  size: string;
+  product_id: string;
+  side: string;
+  stp: string;
+  order_type: string [@key "type"];
+  time_in_force: string;
+  post_only: bool;
+  created_at: string;
+  fill_fees: string;
+  filled_size: string;
+  executed_value: string;
+  status: string;
+  settled: bool;
+} [@@deriving show, yojson]
+
+type product = {
+  id: string;
+  display_name: string;
+  base_currency: string;
+  quote_currency: string;
+  base_increment: string;
+  quote_increment: string;
+  base_min_size: string;
+  base_max_size: string;
+  min_market_funds: string;
+  max_market_funds: string;
+  status: string;
+  status_message: string;
+  cancel_only: bool;
+  limit_only: bool;
+  post_only: bool;
+  trading_disabled: bool;
+} [@@deriving show, yojson {strict = false}]
+
+type order_req = {
+  side: string;
+  product_id: string;
+  price: string;
+  size: string;
+} [@@deriving show, yojson {strict = false}]
+
 type account_list = account list [@@deriving show, yojson]
+type product_list = product list [@@deriving show, yojson]
+type account_history_list = account_history list [@@deriving show, yojson]
 
 let host_from_env = match Sys.getenv_opt ("COINBASE_ENV") with 
   | Some "PROD" -> ""
@@ -39,12 +106,18 @@ let api_params_from_env =
 let hmac_sha256 ~key v =
   Base64.encode_exn Digestif.SHA256.(hmac_string ~key v |> to_raw_string)
 
-let get_accounts =
-  let path = "/accounts" in
-  let req_body = "" in
-  let _method = "GET" in
+type http_method = 
+  | GET
+  | POST
+  | DELETE
+  [@@deriving show { with_path = false } ]
+
+let http http_method path body unmarshaller =
+  let body = match body with 
+  | Some b -> b
+  | None -> "" in
   let timestamp = int_of_float (Unix.time ()) |> string_of_int in 
-  let pre_hash = timestamp ^ _method ^ path ^ req_body in
+  let pre_hash = timestamp ^ (show_http_method http_method) ^ path ^ body in
   let hmac_digest = hmac_sha256 ~key:api_params_from_env.secret pre_hash in 
   let headers = Header.init () in 
   let headers = Header.add headers "CB-ACCESS-KEY" api_params_from_env.key in
@@ -52,11 +125,46 @@ let get_accounts =
   let headers = Header.add headers "CB-ACCESS-TIMESTAMP" timestamp in
   let headers = Header.add headers "CB-ACCESS-SIGN" hmac_digest in
   let uri = Uri.of_string (api_params_from_env.api_host ^ path) in
-  Client.get ~headers:headers uri >>= fun (resp, body) ->
+  let result = match http_method with 
+  | POST -> Client.post ~body:(Cohttp_lwt.Body.of_string body) ~headers uri
+  | DELETE -> Client.delete ~headers uri
+  | GET -> Client.get ~headers uri in  
+  result >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   body |> Cohttp_lwt.Body.to_string >|= fun body ->
   let json = Yojson.Safe.from_string body in 
   match code with 
-  | 200 -> account_list_of_yojson json
+  | 200 -> unmarshaller json
   | _ -> Result.Error (Printf.sprintf "Unexpected response code: %d" code)
-  
+
+let http_get path unmarshaller = 
+  http GET path None unmarshaller
+
+let http_del path = 
+  let unit_unmarshall (t: Safe.t) = match t with 
+  | _ -> Result.Ok () in
+  http DELETE path None unit_unmarshall
+
+let http_post ~path ~body ~unmarshaller = 
+  http POST path body unmarshaller
+
+let get_products = 
+  http_get "/products" product_list_of_yojson
+
+let get_account_history id = 
+  http_get ("/accounts/" ^ id ^ "/ledger") account_history_list_of_yojson
+
+let get_accounts = 
+  http_get "/accounts" account_list_of_yojson
+
+let get_account id = 
+  http_get ("/account/" ^ id) account_of_yojson
+
+let get_product id = 
+  http_get ("/products/" ^ id) product_of_yojson
+
+let create_order order_req = 
+  http_post ~path:"/orders" ~body:(order_req_to_yojson order_req |> Yojson.Safe.to_string |> Some) ~unmarshaller:order_of_yojson
+
+let cancel_order id = 
+  http_del ("/orders/" ^ id)
